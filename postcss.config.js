bc2f2e5575dcd6eb7a248cefa90f158af7efa599
Stefan VanBuren <vanburenstefan@gmail.com>
2020-03-07 15:37:15 -0500

    Remove whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,7 @@ module.exports = {
     // Removes unused CSS
     // https://github.com/FullHuman/purgecss/tree/master/packages/postcss-purgecss
     "@fullhuman/postcss-purgecss": {
-      content: ["layouts/**/*.html"],
-      // TODO: Audit this list
-      whitelist: ["highlight", "pre", "code", "content", "h3", "h4", "ul", "li"]
+      content: ["layouts/**/*.html"]
     },
     // Add vendor prefixes for browsers
     // https://github.com/postcss/autoprefixer
