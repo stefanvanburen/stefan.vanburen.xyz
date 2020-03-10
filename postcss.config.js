d5a0dcf653a4e3d89cc155bc81ab7bbbb33bb0b5
Stefan VanBuren <vanburenstefan@gmail.com>
2020-03-10 08:59:45 -0400

    Test postcss + purgecss (#22)

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    // Removes unused CSS
+    // https://github.com/FullHuman/purgecss/tree/master/packages/postcss-purgecss
+    "@fullhuman/postcss-purgecss": {
+      content: ["layouts/**/*.html"]
+    },
+    // Add vendor prefixes for browsers
+    // https://github.com/postcss/autoprefixer
+    autoprefixer: {},
+    // minifies css
+    // https://cssnano.co/
+    cssnano: { preset: "default" }
+  }
+};
