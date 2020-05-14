bf84a5b0dc9ba4249bd2147bbc42b7231d5725ab
Stefan VanBuren <vanburenstefan@gmail.com>
2020-05-13 21:19:07 -0400

    Rework CSS
    PurgeCSS is removing the <pre> CSS because it doesn't exist in the html
    files until rendered. Ugh.

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,10 @@
 const postcssimport = require("postcss-import");
-const purgecss = require("@fullhuman/postcss-purgecss");
 const darkTheme = require("postcss-dark-theme-class");
 
 module.exports = {
   plugins: [
     // parses `import()`s
     postcssimport(),
-    // Removes unused CSS
-    // https://github.com/FullHuman/purgecss/tree/master/packages/postcss-purgecss
-    purgecss({
-      content: ["./layouts/**/*.html"]
-    }),
     darkTheme({
       darkSelector: '[data-theme="dark"]',
       lightSelector: '[data-theme="light"]'
