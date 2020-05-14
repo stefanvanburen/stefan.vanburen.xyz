7360cf6a8479615f834e161e9e327c5281ff7e37
Stefan VanBuren <vanburenstefan@gmail.com>
2020-05-14 07:37:11 -0400

    Further flesh out dark mode initial state

--- static/colors.js
+++ static/colors.js
@@ -1,7 +1,27 @@
 const html = document.documentElement;
-
 const darkButton = document.querySelector("[data-set-theme-dark]");
 const lightButton = document.querySelector("[data-set-theme-light]");
+
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (darkModeMediaQuery.matches) {
+  lightButton.classList.toggle("dn", false);
+} else {
+  darkButton.classList.toggle("dn", false);
+}
+
+darkModeMediaQuery.addListener(e => {
+  const darkModeOn = e.matches;
+  if (darkModeOn) {
+    html.dataset.theme = "dark";
+    darkButton.classList.toggle("dn", true);
+    lightButton.classList.toggle("dn", false);
+  } else {
+    html.dataset.theme = "light";
+    darkButton.classList.toggle("dn", false);
+    lightButton.classList.toggle("dn", true);
+  }
+});
 
 darkButton.addEventListener("click", () => {
   html.dataset.theme = "dark";
@@ -16,9 +36,3 @@ lightButton.addEventListener("click", () => {
   darkButton.classList.toggle("dn", false);
   lightButton.classList.toggle("dn", true);
 });
-
-if (html.dataset.theme === "light") {
-  darkButton.classList.toggle("dn", false);
-} else {
-  lightButton.classList.toggle("dn", false);
-}
