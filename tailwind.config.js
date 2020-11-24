a03dd4b08101501012b3a3588ed22189a6e2ca57
Stefan VanBuren <me@svbn.me>
2020-11-23 19:55:30 -0500

    chore: Disable some tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 // tailwind.config.js
 module.exports = {
-  purge: [
-     './layouts/**/*.html',
-  ],
+  purge: {
+    // To enable purging in development, uncomment this
+    // enabled: true,
+    content: ['./layouts/**/*.html'],
+  },
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
+  corePlugins: {
+    preflight: false,
+    animation: false,
+  }
 }
